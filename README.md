9f62ec9acc7874290684806fdfd7dd15a4d18601
Fernando Fraga <fernando2015fraga@gmail.com>
2021-05-24 17:09:08 -0300

    [Update] Inserção do passo 2 da calculadora

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Projeto criado para praticar HTML, CSS E JS e tem como objetivo construir uma si
 
 ## Passo 1 - Design HTML E CSS
 Nesse passo deverá ser realizado o processo da criação da interface, e ela deve seguir os seguintes requisitos:
- - O display da calculadora deve ser uma tag input
- - O teclado da calculadora deve possuir todos as 4 operações (+, -, /, *), os algaritmos numéricos (0, 1, 2, 3, 4, 5, 6, 7, 8, 9), e os botões extras (C, AC, .)
+ - O display da calculadora deve ser uma tag input.
+ - O teclado da calculadora deve possuir todos as 4 operações (+, -, /, *), os algaritmos numéricos (0, 1, 2, 3, 4, 5, 6, 7, 8, 9), e os botões extras (C, AC, .).
  - É recomendado a não utilização de nenhum framework para que o processo de desenvolvimento seja orgânico.
  - Os botões devem seguir um padrão de estrutura para facilitar as chamadas em JS posteriormente.
+
+## Passo 2 - Desenvolver lógica
+Nesse passo deverá ser realizado todo o processo de criação da lógica por trás da calculadora, o funcionamento esperado segue os seguintes requisitos:
+- A calculadora deve realizar as 4 operações básicas (Adição, subtração, divisão e multiplicação).
+- A calculadora deve possuir a função de limpar somente o último digito e também limpar toda a operação.
+- A calculadora deve preservar o resultado da última operação para que esse possa ser utilizado na próxima operação.
+
+**Obs: Fica a encargo do desenvolvedor escolher se irá utilizar ou não jquery para auxiliar no desenvolvimento.**
---
+++ js/app.js
@@ -0,0 +1,130 @@
+var app = []; 			//Variaveis de controle
+app.display   = '0';
+app.keyDown   = true;
+app.acc       = 0;		//Variável acumulador
+app.operation = '';
+
+//Controladora de Eventos
+$("#keyboard td").click(function(){ //Captura eventos de cliques nas tags <td>
+	var value = $(this).text(); //Armazena valor de dentro do td em uma variavel
+
+	if ($.isNumeric(value)) { // Filtra os botões núméricos dos botões de ação
+		keyNumeric(value);
+
+	} else if (value == '.'){
+		keyDecimal();
+
+	} else {
+		switch(value) {
+			case 'C':
+				clear();
+				break;
+			case 'AC':
+				allClear();
+				break;
+			case '=':
+				calculate();
+				break;
+			default:
+				keyOperation(value);
+				break;
+		}
+	}
+
+	print();
+});
+
+function keyNumeric(value){
+	if (app.keyDown == false) {
+		app.keyDown = true;
+		app.display = "0";
+	}
+
+	if (app.display == '0') { //Impede que sejam adicionados zeros a esquerda
+		app.display = String(value);
+	} else {
+		app.display += String(value);
+	}
+}
+
+function keyDecimal(){
+	if (app.keyDown) {
+		if (app.display.indexOf('.') == -1) { //Checa se a string já tem um .
+			app.display += '.';
+		}
+	}
+}
+
+function keyOperation(value) {
+	if (app.operation == '') {
+		app.acc       = parseFloat(app.display); 
+	} else {
+		switch(app.operation) { //switch para realizar as operações
+			case '+':
+				app.acc = parseFloat(app.acc) + parseFloat(app.display);
+				app.display = app.acc;
+				break;
+			case '-':
+				app.acc = parseFloat(app.acc) - parseFloat(app.display);
+				app.display = app.acc;
+				break;
+			case 'X':
+				app.acc = parseFloat(app.acc) * parseFloat(app.display);
+				app.display = app.acc;
+				break;
+			case '/':
+				app.acc = parseFloat(app.acc) / parseFloat(app.display);
+				app.display = app.acc;
+				break;
+		}
+	}
+	app.operation = value;
+	app.keyDown   = false;
+}
+
+function calculate(){
+	if (app.operation != '') {
+		switch(app.operation) {
+			case '+':
+				app.acc = parseFloat(app.acc) + parseFloat(app.display);
+				app.display = app.acc;
+				break;
+			case '-':
+				app.acc = parseFloat(app.acc) - parseFloat(app.display);
+				app.display = app.acc;
+				break;
+			case 'X':
+				app.acc = parseFloat(app.acc) * parseFloat(app.display);
+				app.display = app.acc;
+				break;
+			case '/':
+				app.acc = parseFloat(app.acc) / parseFloat(app.display);
+				app.display = app.acc;
+				break;
+		}
+		app.operation = '';
+		app.keyDown   = false;
+	}
+}
+
+function print(){ //Responsável por atualizar o display
+	$("#display input").val(app.display); //Injeta o valor da váriavel de controle no display
+	$("#display span").text(app.operation); //Injeta o valor de operação no display
+}
+
+function clear(){ //Limpa o último digito
+	if (app.keyDown) {
+		app.display = app.display.slice(0, -1); //Retira último caractere da string
+		if (app.display.charAt(app.display.length - 1) == '.') { //Testa pra ver se o último caractere é um .
+			app.display = app.display.slice(0, -1); //Retira o ponto caso TRUE
+		}
+
+		app.display = (app.display == '') ? "0" : app.display; // Operador ternário para limitar o delete no último digito
+	}
+}
+
+function allClear(){ //Limpa tudo
+	app.display   = '0';
+	app.operation = '';
+	app.acc       = 0;
+}
